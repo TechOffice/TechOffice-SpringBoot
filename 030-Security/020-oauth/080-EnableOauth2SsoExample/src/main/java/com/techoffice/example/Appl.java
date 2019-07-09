d6810556a6d545639545a124de62d13b9625900f
ben <ben@techoffice.com>
2019-07-09 08:03:51 +0800

    update

---
+++ 030-Security/020-oauth/080-EnableOauth2SsoExample/src/main/java/com/techoffice/example/Appl.java
@@ -0,0 +1,22 @@
+package com.techoffice.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableOAuth2Sso
+@SpringBootApplication
+class Appl {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args){
+        SpringApplication.run(Appl.class, args);
+	}
+
+}
