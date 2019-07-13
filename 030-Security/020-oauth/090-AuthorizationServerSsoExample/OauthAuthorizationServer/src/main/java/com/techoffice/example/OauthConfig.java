73667a61691427adcf0330461b0e8d71be664bf3
ben <ben@techoffice.com>
2019-07-13 21:14:55 +0800

    update

---
+++ 030-Security/020-oauth/090-AuthorizationServerSsoExample/OauthAuthorizationServer/src/main/java/com/techoffice/example/OauthConfig.java
@@ -0,0 +1,38 @@
+package com.techoffice.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@EnableAuthorizationServer
+@Configuration
+@Order(1)
+public class OauthConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .authorizedGrantTypes("password")
+                .secret("{noop}secret")
+                .scopes("all");
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+            throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+
+
+}
