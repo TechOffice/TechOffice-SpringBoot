997994c31623836a30d87c4fa7d881990619d050
ben <imben1109@gmail.com>
2019-12-15 03:39:06 +1400

    update

---
+++ 080-Integration/010-SpringBootExample/src/main/java/com/techoffice/example/CustomFileReadingMessageSource.java
@@ -0,0 +1,9 @@
+package com.techoffice.example;
+
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomFileReadingMessageSource extends FileReadingMessageSource {
+
+}
---
+++ 090-Others/010-Drools/010-SpringBootExample/src/main/java/com/techoffice/config/DroolsConfig.java
@@ -0,0 +1,32 @@
+package com.techoffice.config;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DroolsConfig {
+
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+        kieFileSystem.write(ResourceFactory.newClassPathResource(""));
+        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+        kieBuilder.buildAll();
+        KieModule kieModule = kieBuilder.getKieModule();
+        return kieServices.newKieContainer(kieModule.getReleaseId());
+    }
+
+    @Bean
+    public StatelessKieSession statelessKieSession(){
+        return kieContainer().newStatelessKieSession();
+    }
+
+}
