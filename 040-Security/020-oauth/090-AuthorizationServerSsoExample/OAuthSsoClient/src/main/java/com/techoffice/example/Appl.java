7e8180499b2c285d688dde479045b9a8fd59f35a
ben <imben1109@gmail.com>
2020-01-06 00:43:26 +0800

    update

--- 040-Security/020-oauth/090-AuthorizationServerSsoExample/OAuthSsoClient/src/main/java/com/techoffice/example/Appl.java
+++ 040-Security/020-oauth/090-AuthorizationServerSsoExample/OAuthSsoClient/src/main/java/com/techoffice/example/Appl.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-class Appl {
+public class Appl {
 
 	@Bean
 	public RestTemplate restTemplate() {
