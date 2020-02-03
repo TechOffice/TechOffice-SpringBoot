3c90b5bbef5fe11ae148902851b74b82fa5a3496
ben <imben1109@gmail.com>
2020-02-03 23:22:47 +0800

    update oauth example batch script

--- 040-Security/020-oauth/090-AuthorizationServerSsoExample/start-auth-server.bat
+++ 040-Security/020-oauth/090-AuthorizationServerSsoExample/start-auth-server.bat
@@ -1,1 +1,3 @@
+@echo off
+title auth-server
 java -jar ./OauthAuthorizationServer/target/auth-server-0.0.1-SNAPSHOT.jar
--- 040-Security/020-oauth/090-AuthorizationServerSsoExample/start-sso-client.bat
+++ 040-Security/020-oauth/090-AuthorizationServerSsoExample/start-sso-client.bat
@@ -1,1 +1,3 @@
+@echo off
+title sso-client
 java -jar ./OAuthSsoClient/target/sso-client-0.0.1-SNAPSHOT.jar
