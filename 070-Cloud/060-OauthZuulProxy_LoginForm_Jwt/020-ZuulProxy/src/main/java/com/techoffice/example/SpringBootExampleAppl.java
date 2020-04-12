9ca28311fcfae34944907893d8e6b13c2d3ee6fd
ben <imben1109@gmail.com>
2020-04-12 10:01:48 +0800

    update

---
+++ 070-Cloud/060-OauthZuulProxy_LoginForm_Jwt/020-ZuulProxy/src/main/java/com/techoffice/example/SpringBootExampleAppl.java
@@ -0,0 +1,34 @@
+package com.techoffice.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootExampleAppl {
+
+	@RequestMapping("/")
+    String home() {
+        return "Proxy: Hello World!";
+    }
+
+    @RequestMapping("/test")
+    String test() {
+        return "Proxy: Hello World Test!";
+    }
+	
+	public static void main(String[] args){
+        SpringApplication.run(SpringBootExampleAppl.class, args);
+	}
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
+
+}
