df65fc4de73ff39cfdc068fd0477102a6949d841
ben <imben1109@gmail.com>
2020-04-19 17:30:17 +0800

    update

--- 050-Security/020-oauth/100-AuthorizationServerSsoZuulExample/start-auth-server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-title auth-server
-java -jar ./OauthAuthorizationServer/target/auth-server-0.0.1-SNAPSHOT.jar
--- 050-Security/020-oauth/100-AuthorizationServerSsoZuulExample/start-sso-client.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-title sso-client
-java -jar ./OAuthSsoClient/target/sso-client-0.0.1-SNAPSHOT.jar
