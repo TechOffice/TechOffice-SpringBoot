38bec4c3b0e6997f13c773756df5ea33fc26c0b3
ben <imben1109@gmail.com>
2020-04-19 10:29:07 +0800

    update example

--- 070-Cloud/060-OauthZuulProxy_jwt/010-OauthServer/src/main/java/com/techoffice/example/config/OauthConfig.java
+++ 070-Cloud/060-OauthZuulProxy_jwt/010-OauthServer/src/main/java/com/techoffice/example/config/OauthConfig.java
@@ -13,7 +13,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @EnableAuthorizationServer
 @Configuration
@@ -25,6 +27,8 @@ public class OauthConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    private static final String SIGN_KEY = "testing";
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -46,7 +50,22 @@ public class OauthConfig extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints)
             throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                .accessTokenConverter(accessTokenConverter())
+                .tokenStore(tokenStore());
     }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGN_KEY);
+        return converter;
+    }
+
 
 }
--- 070-Cloud/060-OauthZuulProxy_jwt/020-ZuulProxy/src/main/java/com/techoffice/SpringBootExampleAppl.java
+++ 070-Cloud/060-OauthZuulProxy_jwt/020-ZuulProxy/src/main/java/com/techoffice/SpringBootExampleAppl.java
@@ -2,7 +2,6 @@ package com.techoffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
